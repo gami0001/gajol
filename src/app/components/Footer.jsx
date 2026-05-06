eeb2c8e26be2fe2515b41af77b18edd44942966a
josefine-krarup <joskr0005@stud.kea.dk>
2026-05-06 13:50:13 +0200

    footer midtvejs

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,23 @@
 const Footer = () => {
   return (
-    <div>
-      <h2>
-        NIGHT<span>CLUB</span>
-      </h2>
-      <h3>HAVE A GOOD TIME</h3>
-      <h4>LOCATION</h4>
-      <h5>Kompagnistræde</h5>
+    <div className="flex flex-col">
+      <div>
+        <h2>
+          NIGHT<span className="text-pink-700">CLUB</span>
+        </h2>
+        <h3>HAVE A GOOD TIME</h3>
+        <h4 className="text-pink-700">LOCATION</h4>
+        <p>Kompagnistræde 278</p>
+        <p>1265 København K</p>
+        <h4 className="text-pink-700">OPENING HOURS</h4>
+        <p>WED - THU 10:30 PMTO 3 AM</p>
+        <p>SAT - SUN: 11 PM TO 5 AM</p>
+        <p>Stay Connected With Us</p>
+      </div>
+      <div>
+        <h4 className="text-pink-700">NEWS</h4>
+        <h4 className="text-pink-700">RECENT POSTS</h4>
+      </div>
     </div>
   );
 };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import "./globals.css";
 import Footer from "./components/Footer";
 
 export default function RootLayout() {
