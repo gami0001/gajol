fb20c71cb2ba971edcca335e4fc33f900ee83c43
gami0001 <gami0001@stud.kea.dk>
2026-05-06 14:15:32 +0200

    nav snart done

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,11 +1,11 @@
 const Nav = () => {
   return (
     <header className="flex justify-between items-center px-30 py-4 border-b sticky top-0 z-50">
-      <div className="flex flex-col">
+      <div className="flex flex-col tracking-[0.12em]">
         <h2 className="text-2xl font-bold">
           NIGHT<span className="text-pink-500">CLUB</span>
         </h2>
-        <h3 className="text-sm text-gray-500 tracking-wide">HAVE A GOOD TIME</h3>
+        <h3 className="text-[11px] tracking-[0.3em]">HAVE A GOOD TIME</h3>
       </div>
 
       <nav>
