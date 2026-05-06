796323da58c84f4d5f38032446c391349d0501ea
gami0001 <gami0001@stud.kea.dk>
2026-05-06 13:50:04 +0200

    midtvej

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 export default function RootLayout() {
   return (
     <html>
       <body>
+        <Nav />
         <Footer />
       </body>
     </html>
