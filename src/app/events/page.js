47e41d0b2bb12bb88d453ae49e052a78bf2e3374
loje0003 <loje0003@stud.kea.dk>
2026-05-07 17:21:10 +0200

    hero-done

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,8 +1,11 @@
 import EventList from "@/components/EventList";
+import Hero from "@/components/Hero";
 import Image from "next/image";
+
 const Events = () => {
   return (
     <div>
+      <Hero text="EVENTS" />
       <EventList />
     </div>
   );
