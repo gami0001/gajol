da14ed14e6b15cbfd219d84797358a43a4a0a179
gami0001 <gami0001@stud.kea.dk>
2026-05-07 16:39:20 +0200

    done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,19 @@
 const Nav = () => {
   return (
-    <header className="flex justify-between items-center px-30 py-4 border-y sticky top-0 z-50 border-primary-500">
+    <header className="flex justify-between items-center px-30 py-4 border-y sticky top-0 z-50 border-primary-500" style={{ background: "var(--background)" }}>
+      <div
+        className="absolute top-0 left-0 w-10 h-10 bg-primary-500"
+        style={{
+          clipPath: "polygon(0 0, 100% 0, 0 100%)",
+        }}
+      />
+
+      <div
+        className="absolute bottom-0 right-0 w-15 h-15 bg-primary-500"
+        style={{
+          clipPath: "polygon(100% 100%, 100% 0, 0 100%)",
+        }}
+      />
       <div className="flex flex-col tracking-[0.13em]">
         <h2 className="text-2xl font-bold">
           NIGHT<span className="text-primary-500">CLUB</span>
