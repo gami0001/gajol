256023f42f284fce90042c3af87494f26ca24934
gami0001 <gami0001@stud.kea.dk>
2026-05-07 16:16:19 +0200

    newsletter mangler knap

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,11 @@ export default function Home() {
     <main>
       <EventList />
       <div>
-        <h2>WANT THE LATEST CLUB NEWS?</h2>
+        <h2 className="text-center">WANT THE LATEST CLUB NEWS?</h2>
+        <h3 className="text-center">
+          Subscribe to our newsletter and never miss an <span className="text-primary-500">Event</span>
+        </h3>
+        <input className="border-b-2" type="email" id="email" pattern=".+@example\.com" size="30" required placeholder="Enter your email" />
       </div>
     </main>
   );
