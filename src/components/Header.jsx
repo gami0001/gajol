29f66697304829b643d15b5c16523ae24e772bc7
josefine-krarup <joskr0005@stud.kea.dk>
2026-05-07 18:07:07 +0200

    header done done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,10 @@ const Header = () => {
     <div className="relative w-full h-150">
       <Image src="/assets/bg/header_bg_2.jpg" alt="background" fill className="object-cover" />
 
+      {/* overlay */}
       <div className="absolute inset-0 bg-black-70" />
 
+      {/* centered text */}
       <div className="absolute inset-0 flex flex-col items-center justify-center gap-5">
         <Image src="/assets/icon/Logo.svg" alt="logo" width={700} height={30}></Image>
         <h3 className="text-[30px] tracking-[0.65em]">HAVE A GOOD TIME</h3>
