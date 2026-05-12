38e31370745d97faff7de2613d8055ba19ecd29b
gami0001 <gami0001@stud.kea.dk>
2026-05-12 14:29:06 +0200

    uppercase ret done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Hero = ({ text = "OVERSKRIFT" }) => {
   return (
-    <div className="relative w-full h-50">
+    <div className="relative w-full h-40">
       <Image src="/assets/bg/footerbg.jpg" alt="background" fill className="object-cover" />
 
       {/* overlay */}
@@ -10,7 +10,7 @@ const Hero = ({ text = "OVERSKRIFT" }) => {
 
       {/* centered text */}
       <div className="absolute inset-0 flex flex-col items-center justify-center">
-        <h1 className="text-white md:text-3xl uppercase">{text}</h1>
+        <h1 className="text-white text-[40px] md:text-3xl uppercase">{text}</h1>
 
         <Image src="/assets/bottom_line2.png" alt="bottom line" width={200} height={20} />
       </div>
