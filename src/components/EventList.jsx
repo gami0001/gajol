b6c1c61b146edcd079b764faa678772ed34de48d
loje0003 <loje0003@stud.kea.dk>
2026-05-19 11:57:10 +0200

    sideSkiftEventDone

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,30 +1,55 @@
-import { Suspense } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import CardEvent from "./CardEvent";
 
-const Events = () => {
+const EVENTS_PER_PAGE = 3;
+
+const EventList = () => {
+  const [events, setEvents] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await fetch("https://nightclub2026.onrender.com/events");
+        const data = await response.json();
+
+        setEvents(data);
+      } catch (error) {
+        console.error("Failed to load events");
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  // Pagination logic
+  const startIndex = (currentPage - 1) * EVENTS_PER_PAGE;
+  const endIndex = startIndex + EVENTS_PER_PAGE;
+
+  const currentEvents = events.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(events.length / EVENTS_PER_PAGE);
+
   return (
-    <div>
-      <Suspense fallback={<p>Loading events…</p>}>
-        <FetchEvent />
-      </Suspense>
+    <div className="flex flex-col gap-12 mb-15">
+      {/* EVENTS */}
+      {currentEvents.map((event, index) => (
+        <CardEvent key={event.id} id={event.id} title={event.title} date={event.date} location={event.location} description={event.description} asset={event.asset.url} index={index} />
+      ))}
+
+      <div className="flex justify-center items-center gap-8 mt-10 text-lg">
+        {/* Page numbers */}
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button key={i} onClick={() => setCurrentPage(i + 1)} className={`transition pb-1 ${currentPage === i + 1 ? "border-b border-white" : ""}`}>
+            {i + 1}
+          </button>
+        ))}
+        {currentPage < totalPages && <button onClick={() => setCurrentPage(currentPage + 1)}>næste &gt;</button>}
+      </div>
     </div>
   );
 };
-const FetchEvent = async () => {
-  "use server";
 
-  var url = `https://nightclub2026.onrender.com/events`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return data.map((event, index) => {
-      return <CardEvent key={event.id} id={event.id} title={event.title} date={event.date} location={event.location} description={event.description} asset={event.asset.url} index={index} />;
-    });
-  } catch (error) {
-    return <p>Failed to load events. Please try again later.</p>;
-  }
-};
-
-export default Events;
+export default EventList;
